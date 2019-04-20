49aa2f298cdc116b8ced253378083d2b2425f465
Aphizit <gotenya@hotmail.com>
2019-04-21 00:02:08 +0700

    Update content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { FaFacebook, FaGithub,FaTwitterSquare,FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaGithub,FaTwitterSquare,FaInstagram,FaStar,FaStarHalfAlt,FaRegStar } from 'react-icons/fa';
 
 class App extends Component {
   render() {
@@ -21,13 +21,55 @@ class App extends Component {
           </div>
           <h1><FaGithub /><FaFacebook/><FaTwitterSquare/><FaInstagram/></h1>
           <div className="title">Profile</div>
+          <p>I like to solve problem and learn new things.</p>
+          <p>And I like to share knowledge when I learn new things</p>
+          <p>I am happy when I develop and maintain some products that can solve real problem.</p>
           <div className="title">Technical Skills</div>
+          <p>JavaScript</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
+          </div>
+          <p>HTML/CSS</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaStar/><FaStarHalfAlt/><FaRegStar/>
+          </div>
+          <p>C#</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaStarHalfAlt/><FaRegStar/><FaRegStar/>
+          </div>
+          <p>VB.net</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaStarHalfAlt/><FaRegStar/><FaRegStar/>
+          </div>
+          <p>Python</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaRegStar/><FaRegStar/><FaRegStar/>
+          </div>
+          <br/>
           <div className="title">Language Skills</div>
+          <p>Thai (Native)</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/>
+          </div>
+          <p>English</p>
+          <div className="star">
+            <FaStar/><FaStar/><FaStarHalfAlt/><FaRegStar/><FaRegStar/>
+          </div>
         </div>
         <div className="app-right">
           <div className="title">Experience</div>
+          <div className="exHeader">
+            <h2 className="exTitle">Sansiri</h2><p className="exTime">March 2018 - March 2019</p>
+          </div>
+          <ul>
+            <li className="exListItem">Develop web application with C#, VB.net and NodeJS.</li>
+            <li className="exListItem">Database with SQL.</li>
+          </ul>
           <div className="title">Education</div>
-          <div className="title">Hobby</div>
+          <div className="exHeader">
+            <h3 className="exTitle">Bachelor's Degree in Computer Engineering, Mahanakorn University of Technology</h3><p className="exTime"></p>
+          </div>
+          <div className="title">Interests</div>
         </div>       
       </div>
     );
